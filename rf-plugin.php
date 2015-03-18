71bacf57f863c6fa6e231dfff7ec1b34b193e611
Jacob Williams <jwilliams@launchbrigade.com>
2015-03-18 12:43:47 -0700

    Created basic files

---
+++ rf-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: RF Theme Plugin
+ * Plugin URI: https://github.com/LaunchBrigade/rf-plugin
+ * Description: Implements added functionality to WordPress required by the Rocket Fuel theme.
+ * Version: 0.1.0
+ * Author: Launch Brigade
+ * Author URI: https://www.launchbrigade.com/
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
